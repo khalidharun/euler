2b0b33a5a0b2fc430fbdec93733384a2b0e32624
Khalid Harun <khalid.harun@avant.com>
2016-02-07 01:19:57 -0600

    more optimizations to the prime number generation

--- 007/nth_prime2.R
+++ 007/nth_prime2.R
@@ -1,82 +1,92 @@
-find_nth_prime <- function(N) {
+create_new_bucket <- function(lo, size, env) {
+  if (lo %% 2 == 0) stop("lo must be an odd number")
 
-  create_new_bucket <- function(lo, size) {
-    hi <- lo + size - 1
-    paste("Creating bucket: (", lo, "...", hi, ")") %>% message
-    setNames(!vector("logical", size), seq(lo, hi))
+  hi <- lo + 2*(size - 1)
+  paste("Creating bucket: (", lo, "...", hi, ")") %>% message
+  env$bucket <- setNames(!vector("logical", size), seq(lo, hi, 2))
+  env$hi <- hi
+  env$lo <- lo
+  env$bucket
+}
+
+
+mark_composites <- function(env, p) {
+  hi <- env$hi
+  lo <- env$lo
+  from <- p * p
+  if (hi < from) return(FALSE)
+
+  # message("Marking ", paste(from, p, hi, sep = ":"))
+  idx <- intersect(names(env$bucket), as.character(seq(from, hi, p)))
+  env$bucket[idx] <- FALSE
+  TRUE
+}
+
+
+
+get_next_prime <- function(env = prime_env) {
+
+  p <- NULL
+  while (is.null(p)) {
+    p <- find_next_prime(env)
+  }
+  message("Found prime #", length(env$primes), ": ", p)
+  p
+}
+
+find_next_prime <- function(env) {
+  # on.exit({message("find_next_prime: ", returnValue())})
+
+  if (is.null(env$cursor)) {
+    p <- 2
+    env$primes <- p
+    create_new_bucket(3, getOption("prime.bucket_size", 2^11), env)
+    env$cursor <- p + 1 # start with 3
+    mark_composites(env, p)
+    return(p)
   }
 
-  env <- list2env(
-    list(
-      primes = NULL,
-      bucket = create_new_bucket(2, 2^12),
-      cursor = 1
-    )
-  )
-
-  get_next_prime <- function() {
-    p <- NULL
-    while (is.null(p)) {
-      p <- find_next_prime()
-      if (is.null(p)) {
-        size <- length(env$bucket)
-        lo <- names(tail(env$bucket, 1)) %>% as.numeric + 1
-        use_next_bucket(lo, size)
-      }
-    }
-    message("Found prime #", length(env$primes), ": ", p)
-    p
-  }
-
-  append_prime <- function(p) {
-    env$primes <- c(env$primes, p)
-  }
-
-  find_next_prime <- function() {
-    # on.exit({message("find_next_prime: ", returnValue())})
-
-    if (env$cursor == 1) {
-      env$cursor <- 2
-      append_prime(env$cursor)
-      return(2)
-    }
-
-    mark_composites(env$cursor)
-    hi <- names(tail(env$bucket, 1)) %>% as.numeric
-
-    if (env$cursor >= hi) return(NULL)
-
-    for (i in seq(env$cursor+1, hi, 1)) {
+  if (env$cursor <= env$hi) {
+    for (i in seq(env$cursor, env$hi, 2)) {
       if (isTRUE(env$bucket[[as.character(i)]])) {
-        env$cursor <- i
-        append_prime(i)
+        env$primes <- c(env$primes, i)
+        mark_composites(env, i)
+        env$cursor <- i + 2
+        env$p_limit_hit <- FALSE
         return(i)
       }
     }
-    env$cursor <- hi
-    return(NULL)
   }
 
-  use_next_bucket <- function(lo, size) {
-    env$bucket <- create_new_bucket(lo, size)
-    for (p in env$primes) mark_composites(p)
+  # use_next_bucket
+  env$cursor <- env$hi + 2
+  new_lo <- env$hi + 2
+  size <- length(env$bucket)
+  create_new_bucket(new_lo, size, env)
+  for (p in env$primes) {
+    if (!isTRUE(mark_composites(env, p))) {
+      message('Stopped after: ', p)
+      break
+    }
   }
 
-  mark_composites <- function(p) {
-    hi <- names(tail(env$bucket, 1)) %>% as.numeric
-    lo <- names(head(env$bucket, 1)) %>% as.numeric
-    from <- p * (lo %/% p)
+  return(NULL)
+}
 
-    # message("Marking ", paste(from, p, hi, sep = ":"))
-    idx <- intersect(names(env$bucket), as.character(seq(from, hi, p)))
-    env$bucket[idx] <- FALSE
-  }
+# prime_env <- list2env(
+#   list(
+#     primes = NULL,
+#     bucket = create_new_bucket(2, 2^12),
+#     cursor = 1
+#   )
+# )
 
+find_nth_prime <- function(N, env = new.env()) {
   ## Actually run code
   withr::with_options(c(scipen = 999), {
     i <- 0
     while (i < N) {
-      get_next_prime()
+      p <- get_next_prime(env)
       i <- i + 1
     }
   })
